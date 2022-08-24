131fa26051c818eee9c3e95db1993dea8bf88133
Arif Esat <arif55esat41@gmail.com>
2022-08-25 01:27:07 +0300

    Bunlar benim git komutlarım

---
+++ Git-101.py
@@ -0,0 +1,30 @@
+# Repository: "REPO" revizyon kontrolü altındaki dizinler
+# Branch:
+# Commit:
+# Master:
+# Fork:
+# Pull Request (PR):
+
+
+#git komutları
+# 1) git config = kullanıcı adı ve mail gibi bilgilerin girilmesini sağlayan komuttur.
+# git config --global user.mail"arif55esat41@gmail.com"
+# git config --global user.name"Brion Kerr"
+
+# 2) git init = Bulunduğumuz dizinde .git uzantılı Repo oluşturur
+# 3) git add = staging area alanına (ara katmana eklemek) git add. "tüm dosyalar için"
+# 4) git commit = staging alanından çıkıp safe yapıyot gibi (local Repo'ya kaydetmek)
+#  git commit -m"my first commit message" --> commitler check point alanları gibi düşünebiliriz
+
+ 
+# 5) git status = anlık durumumuzu gösteriyor 
+# 6) git branch = farkllı isimle oluşturulan dal  
+# 7) git checkout = istediğimiz branch'e gitmemizi sağlıyor "Eğer böyle bir branch varsa"
+# 8) git checkout -b = olmayan bir branch2 oluşturup gitmek 
+# 9) git merge = branchleri master ile dengelemek 
+#   " iki dosyada aynı  satırda farklı şeyler yazarsa hata verecek hangisi daha öncelikli diye bize sorar (Yani bir hata vermiş olur)" 
+
+#10) git remote = Local repositoryi uzaktaki repositroy'e bağlamaya yarar.
+#11) git clone = uzak sunucudaki bir projeyi bilgisayarımıza indirmek.
+#12) git pull = uzak sunucudaki değişikliği veya projeyi local bilgisayara çekmek için kullanırız.Projenin uzaktakie en güncela hali
+#13) git push = uzaktak i repoya gönderme
